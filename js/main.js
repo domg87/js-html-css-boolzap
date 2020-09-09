db26a7ec609195451bd7c6e6474af2040120615c
dom <domepiro87@yahoo.it>
2020-09-09 16:46:26 +0200

    correzione

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    $(".send-message").click(
+        function() {
+            sendMessage();
+        }
+    );
+
+    $("#input-message").keyup(
+        function(event) {
+            if(event.which == 13){
+                sendMessage();
+            }
+        }
+    );
+
+});
+
+function sendMessage() {
+    var inputText = $("#input-message").val();
+    
+    if(inputText != "") {
+        var templateMessage = $(".templates . message-row").clone();
+        var date = new Date();
+        var hours = date.getHours();
+        var minute = date.getMinutes();
+        var time = hours + ":" + minute;
+
+        
+        templateMessage.find(".message-text").text("inputText");
+        templateMessage.find(".message-time").text("time");
+        templateMessage.addClass("sent");
+
+        $(".chat").append(templateMessage);
+        $("#input-message").val("");
+    }
+}
